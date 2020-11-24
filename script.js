6e2a2e5da8d9c3f69fbcae3df98ce6483c66b101
Diehl Wierce <johnbrotherwood@gmail.com>
2020-11-25 02:02:09 +0300

    Second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function bgChanger() {
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+    let hex = '0123456789abcdef';
+    let result = '#';
+    for (let i=0; i<6; i++){
+        result += hex[getRandomInt(hex.length)];
+    }
+    document.body.style.background = result;
+}
